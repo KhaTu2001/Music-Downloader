eacb8261385eb5a3e583b8bfbcc57acb6a0a97a4
KhaTu2001 <94450785+KhaTu2001@users.noreply.github.com>
2022-09-08 21:10:18 +0700

    fc

---
+++ app/src/main/java/com/ddona/music_download_ms3_tunk/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.ddona.music_download_ms3_tunk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
